6475073d60c76e086b3d40b3ce1cee78d1b312cd
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2018-09-25 14:32:02 -0600

    Ported code in Gaussian_dynamics Juypter notebook into make_arxiv_results.py

--- jupyter/make_arxiv_results.py
+++ jupyter/make_arxiv_results.py
@@ -9,6 +9,10 @@ from collections import defaultdict
 import os
 import math
 import turchin
+from sklearn.preprocessing import StandardScaler, normalize
+from sklearn.mixture import GaussianMixture as GMM
+from sklearn.decomposition import PCA, FastICA
+from sklearn import linear_model
 
 # read csv/excel data files 
 CC_file = os.path.abspath(os.path.join("./..","data","pnas_data1.csv")) #20 imputed sets
@@ -26,6 +30,7 @@ CC_array = CC_df.loc[:, CC_names].values
 
 # Normalize the data (across 20 imputations, not within each imputation)
 CC_scaled = StandardScaler().fit_transform(CC_array)
+CC_times = CC_df.loc[:, ['Time']].values
 
 # Do a singular value decomposition
 P, D, Q = turchin.svd(CC_scaled)
@@ -48,7 +53,7 @@ i = np.random.randint(numImpute)
 
 # histogram of PC1 for pooled imputations
 num_bins = 50
-n, bins, patches = plt.hist(PC_matrix[:,0], num_bins, normed=1, facecolor='blue', alpha=0.5)
+n, bins, patches = plt.hist(PC_matrix[:,0], num_bins, density=1, facecolor='blue', alpha=0.5)
 plt.title("Pooled over imputations")
 plt.xlabel("Projection onto first Principle Component")
 plt.legend()
@@ -132,3 +137,216 @@ plt.savefig("pc1_vs_time_stacked_by_start.pdf")
 plt.savefig("pc1_vs_time_stacked_by_start.eps")
 plt.savefig("pc1_vs_time_stacked_by_start.png")
 plt.close()
+
+#Gaussian Mixture Model 
+#fit GMM
+gmm = GMM(n_components=2).fit(CC_scaled)
+cov = gmm.covariances_
+prob_distr = gmm.predict_proba(CC_scaled)
+
+# determine to which of the two gaussians each data point belongs by looking at probability distribution 
+if gmm.weights_[0] < gmm.weights_[1]:
+    gauss1_idx = [i for i in range(len(prob_distr)) if prob_distr[i][0] >= prob_distr[i][1]]
+    gauss2_idx = [j for j in range(len(prob_distr)) if prob_distr[j][1] >= prob_distr[j][0]]
+else:
+    gauss1_idx = [i for i in range(len(prob_distr)) if prob_distr[i][0] <= prob_distr[i][1]]
+    gauss2_idx = [j for j in range(len(prob_distr)) if prob_distr[j][1] <= prob_distr[j][0]]
+
+
+gauss1_time = [CC_times[i] for i in gauss1_idx] # time for the first gaussian data
+gauss2_time = [CC_times[j] for j in gauss2_idx] # time for the second gaussian data
+
+gauss1_point = [CC_scaled[i] for i in gauss1_idx] # 9-d data point for the first gaussian
+gauss2_point = [CC_scaled[j] for j in gauss2_idx] # 9-d data point for the second gaussian
+
+def dummy(data, ngas):
+    """
+    Given a gaussian projection data and a list of unique ngas, 
+    """
+    # dummy variables for NGAs for fixed effects
+    dummy = [[1 if CC_df.loc[[point]].NGA.tolist()[0] == nga else 0 for nga in ngas] for point in data]
+    return np.asarray(dummy)
+
+dummy1 = dummy(gauss1_idx, NGAs)
+dummy2 = dummy(gauss2_idx, NGAs)
+
+gmm = GMM(n_components=2).fit(PC_matrix)
+cov = gmm.covariances_
+
+# main eigenvectors for covariances of each gaussians
+eigval1, eigvec1 = np.linalg.eig(cov[0])
+eigval2, eigvec2 = np.linalg.eig(cov[1])
+
+# find the eigenvector corresponding to the largest eigenvalue for each of the two gaussians
+max_eigvec1 = eigvec1[:, np.argmax(max(eigval1))] 
+max_eigvec2 = eigvec2[:, np.argmax(max(eigval2))]
+
+# max_eigvec1 = np.asarray([(i**2)/math.sqrt(sum([k**2 for k in max_eigvec1])) for i in max_eigvec1])
+# max_eigvec2 = np.asarray([(j**2)/math.sqrt(sum([k**2 for k in max_eigvec2])) for j in max_eigvec2])
+
+gauss1_proj = np.matmul(gauss1_point, max_eigvec1)
+gauss2_proj = np.matmul(gauss2_point, max_eigvec2)
+
+gauss1_proj = np.vstack((gauss1_proj.T, dummy1.T)).T
+gauss2_proj = np.vstack((gauss2_proj.T, dummy2.T)).T
+
+assert gauss1_proj.shape[1] == 31
+assert gauss2_proj.shape[1] == 31
+
+# Multiple linear regression over time
+ols1 = linear_model.LinearRegression()
+ols2 = linear_model.LinearRegression()
+model1 = ols1.fit(gauss1_time, gauss1_point)
+model2 = ols2.fit(gauss2_time, gauss2_point)
+
+print("coefficients for the first gaussian: ", model1.coef_)
+print("intercept for the first gaussian: ", model1.intercept_)
+print("coefficients for the second gaussian: ",  model2.coef_)
+print("intercept for the second gaussian: ", model2.intercept_)
+
+# pca components 
+pca = PCA(n_components=9)
+pca.fit(CC_scaled)
+components = pca.components_
+print(components)
+print(pca.components_.T * np.sqrt(pca.explained_variance_))
+
+# calculate angle between two vectors 
+def angle(vec1, vec2):
+    """
+    Given two vectors, compute the angle between the vectors
+    """
+    assert vec1.shape == vec2.shape
+    
+    cos_vec = np.inner(vec1, vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2))
+    angle = math.acos(cos_vec)
+    in_deg = math.degrees(angle)
+    if in_deg >= 90:
+        return (180-in_deg)
+    return in_deg
+
+# angle between the two main eigenvectors 
+# max_eigvec1 = np.asarray([(i**2)/sum([k**2 for k in max_eigvec1]) for i in max_eigvec1])
+# max_eigvec2 = np.asarray([(i**2)/sum([k**2 for k in max_eigvec2]) for i in max_eigvec2])
+
+print("original", angle(max_eigvec1, max_eigvec2))
+angles = []
+for i in range(len(max_eigvec1)):
+    norm1 = [max_eigvec1[j] if j != i else 0.0 for j in range(len(max_eigvec1))]
+    norm2 = [max_eigvec2[j] if j != i else 0.0 for j in range(len(max_eigvec2))]
+    print("angle after dropping %s th component: " %(i+1), angle(np.asarray(norm1), np.asarray(norm2)))
+    angles.append(angle(np.asarray(norm1), np.asarray(norm2)))
+
+
+
+# examine where the angle between the two main eigenvectors for each gaussian comes from
+comp1 = np.matmul(max_eigvec1.T, components)
+norm_comp1 = np.asarray([(i**2)/sum([k**2 for k in comp1]) for i in comp1])
+comp2 = np.matmul(max_eigvec2.T, components)
+norm_comp2 = np.asarray([(j**2)/sum([k**2 for k in comp2]) for j in comp2])
+
+print("main eigenvector for the first Gaussian: \n", norm_comp1)
+print("main eigenvector for the second Gaussian: \n",norm_comp2)
+
+angles = []
+print("original angle", angle(norm_comp1, norm_comp2))
+for i in range(len(norm_comp1)): # angle using only some components
+    norm1 = [norm_comp1[j] if j != i else 0.0 for j in range(len(norm_comp1))]
+    norm2 = [norm_comp2[j] if j != i else 0.0 for j in range(len(norm_comp2))]
+    print("angle after dropping %s th component: " %(i+1), angle(np.asarray(norm1), np.asarray(norm2)))
+    angles.append(angle(np.asarray(norm1), np.asarray(norm2)))
+    
+#plt.plot(range(1,10), angles)
+#plt.show()
+#plt.close()
+
+# flow vectors for each NGAs
+nga_dict = {key:list() for key in NGAs}
+vec_coef1 = list() # coefficients for overall flow vectors for each ngas in the first gaussian
+vec_ic1 = list() # intercept for overall flow vectors for each ngas in the first gaussian
+vec_coef2 = list() # coefficients for overall flow vectors for each ngas in the second gaussian
+vec_ic2 = list() # intercept for overall flow vectors for each ngas in the second gaussian
+
+for idx in range(len(PC_matrix)):
+    nga = CC_df.loc[idx].NGA
+    nga_dict[nga].append((PC_matrix[:,0][idx], PC_matrix[:,1][idx], CC_times[idx][0], idx))            
+
+for i in range(len(NGAs)):  # flow vector for each NGA
+
+    nga_pc1 = [p for p,_,_,_ in nga_dict[NGAs[i]]] 
+    nga_pc2 = [j for _,j,_,_ in nga_dict[NGAs[i]]]
+    nga_time = [k for _,_,k,_ in nga_dict[NGAs[i]]]
+
+    nga_pc1 = [x for _,x,_ in sorted(zip(nga_time, nga_pc1, nga_pc2))]
+    nga_pc2 = [y for _,_,y in sorted(zip(nga_time, nga_pc1, nga_pc2))]
+
+    nga_pc_gauss1 = [[p,j] for p,j,_,t in nga_dict[NGAs[i]] if t in gauss1_idx]
+    nga_pc_gauss2 = [[p,j] for p,j,_,t in nga_dict[NGAs[i]] if t in gauss2_idx]
+
+    nga_time1 = np.asarray([k for _,_,k,t in nga_dict[NGAs[i]] if t in gauss1_idx])
+    nga_time2 = np.asarray([k for _,_,k,t in nga_dict[NGAs[i]] if t in gauss2_idx])
+
+    xfit1 = np.linspace(-6, 4, len(nga_time1))
+    xfit2 = np.linspace(-6, 4, len(nga_time2))
+
+    assert len(nga_pc_gauss1) == len(nga_time1)
+    assert len(nga_pc_gauss2) == len(nga_time2)
+    
+    #fit linear regression 
+    if len(nga_time1) == 0:
+        ols2 = linear_model.LinearRegression()
+        model2 = ols2.fit(nga_time2.reshape(-1,1), nga_pc_gauss2)
+                
+        vec_coef2.append(model2.coef_)
+        vec_ic2.append(model2.intercept_)
+        
+        yfit2 = model2.predict(np.sort(nga_time2).reshape(-1, 1))
+        #plt.plot([p for p, _ in yfit2], [q for _, q in yfit2])
+        
+        vec_coef2.append(model2.coef_)
+        vec_ic2.append(model2.intercept_)
+        
+    elif len(nga_time2) == 0:
+        ols1 = linear_model.LinearRegression()
+        model1 = ols1.fit(nga_time1.reshape(-1,1), nga_pc_gauss1)
+        
+        vec_coef1.append(model1.coef_)
+        vec_ic1.append(model1.intercept_)
+        
+        yfit1 = model1.predict(np.sort(nga_time1).reshape(-1, 1))
+
+        #plt.plot([p for p, _ in yfit1], [q for _, q in yfit1])
+        
+        vec_coef1.append(model1.coef_)
+        vec_ic1.append(model1.intercept_)
+
+    else:
+        ols1 = linear_model.LinearRegression()
+        model1 = ols1.fit(nga_time1.reshape(-1,1), nga_pc_gauss1)
+        ols2 = linear_model.LinearRegression()
+        model2 = ols2.fit(nga_time2.reshape(-1,1), nga_pc_gauss2)
+        
+        vec_coef1.append(model1.coef_)
+        vec_ic1.append(model1.intercept_)
+        vec_coef2.append(model2.coef_)
+        vec_ic2.append(model2.intercept_)
+
+        yfit1 = model1.predict(np.sort(nga_time1).reshape(-1, 1))
+        yfit2 = model2.predict(np.sort(nga_time2).reshape(-1, 1))
+        
+        #plt.plot([p for p, _ in yfit1], [q for _, q in yfit1])
+        #plt.plot([p for p, _ in yfit2], [q for _, q in yfit2])
+
+        vec_coef1.append(model1.coef_)
+        vec_ic1.append(model1.intercept_)
+        vec_coef2.append(model2.coef_)
+        vec_ic2.append(model2.intercept_)
+
+    #plt.scatter(nga_pc1, nga_pc2, s=9, c=range(len(nga_pc1)), cmap = 'Blues')
+    #plt.show()
+    #plt.close()
+    
+gauss1_coef = np.mean(vec_coef1, axis=0)
+gauss1_ic = np.mean(vec_ic1, axis=0)
+gauss2_coef = np.mean(vec_coef2, axis=0)
+gauss2_ic = np.mean(vec_ic2, axis=0)
